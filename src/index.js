48bfb60858f95f8a0fd1173a52ba09d06890f1b6
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2023-02-09 22:09:36 +0200

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!Array.isArray(matrix)) {
+        return [];
+    }
+    const newMatrix = matrix.map((item, i, arr) => {
+        if (i % 2 === 0) {
+            return [...item].sort((a, b) => a - b);
+        } else {
+            return [...item].sort((a, b) => b - a);
+        }
+    });
+    const arr = newMatrix.flat();
+    return arr;
+};
